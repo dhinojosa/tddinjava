3e3bc2fd236d3334b581f50474d66be33e9887db
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-05-02 12:08:53 -0700

    Added More to CaesarShift

--- src/main/java/com/xyzcorp/CaesarShift.java
+++ src/main/java/com/xyzcorp/CaesarShift.java
@@ -2,6 +2,7 @@ package com.xyzcorp;
 
 public class CaesarShift {
 
+	private static final int NUMBER_LETTERS = 'z' - 'a' + 1;
 	private int shift;
 	
 	public CaesarShift(int shift) {
@@ -9,7 +10,14 @@ public class CaesarShift {
 	}
 
 	public String encrypt(String string) {
+		//handle the exception
 		if(string.isEmpty()) return "";
-		return "" + (char)(string.charAt(0) + shift);
+		char c = string.charAt(0);
+		char result = c;
+		if (Character.isAlphabetic(c)) {
+		   char offset = Character.isUpperCase(c) ? 'A' : 'a';
+		   result = (char)((c + shift - offset) % NUMBER_LETTERS + offset);
+		}
+		return "" + result;
 	}
 }
--- src/test/java/com/xyzcorp/CaesarShiftTest.java
+++ src/test/java/com/xyzcorp/CaesarShiftTest.java
@@ -2,52 +2,89 @@ package com.xyzcorp;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class CaesarShiftTest {
-    @Test
-    public void testCaesarShiftOfEmptyStringAnd0() {
-         CaesarShift caesarShift = new CaesarShift(0);
-         String result = caesarShift.encrypt("");
-         assertEquals("", result);
-    }
-    
-    @Test
-    public void testAStringWithOneCharAndShiftOfZero() {
-        CaesarShift caesarShift = new CaesarShift(0);
-        String result = caesarShift.encrypt("a");
-        assertThat(result).isEqualTo("a");
-    }
+	@Test
+	public void testCaesarShiftOfEmptyStringAnd0() {
+		CaesarShift caesarShift = new CaesarShift(0);
+		String result = caesarShift.encrypt("");
+		assertEquals("", result);
+	}
 
-    //NotRedBar
-    @Test
-    public void testEmptyStringWithAShiftOne() {
-    	    CaesarShift caesarShift = new CaesarShift(1);
-        String result = caesarShift.encrypt("");
-        assertThat(result).isEqualTo("");
-    }
-    
-    //NotRedBar
-    @Test
-    public void testEmptyStringWithAShiftTen() {
-    	    CaesarShift caesarShift = new CaesarShift(10);
-        String result = caesarShift.encrypt("");
-        assertThat(result).isEqualTo("");
-    }
-    
-    @Test
-    public void testStringOfOneAlphaCharAndShiftOf1(){
-    	    CaesarShift caesarShift = new CaesarShift(1);
-        String result = caesarShift.encrypt("a");
-        assertThat(result).isEqualTo("b");
-    }
-    
-    
-    public void testStringOfSpace() {}
-    public void testStringOfSpecialChars() {}
-    
-    public void testCaesarShiftOfNull() {}
-    public void testDoWeShiftNumbers() {}
-    public void testNegativeShift() {}   
+	@Test
+	public void testAStringWithOneCharAndShiftOfZero() {
+		CaesarShift caesarShift = new CaesarShift(0);
+		String result = caesarShift.encrypt("a");
+		assertThat(result).isEqualTo("a");
+	}
+
+	// NotRedBar
+	@Test
+	public void testEmptyStringWithAShiftOne() {
+		CaesarShift caesarShift = new CaesarShift(1);
+		String result = caesarShift.encrypt("");
+		assertThat(result).isEqualTo("");
+	}
+
+	// NotRedBar
+	@Test
+	public void testEmptyStringWithAShiftTen() {
+		CaesarShift caesarShift = new CaesarShift(10);
+		String result = caesarShift.encrypt("");
+		assertThat(result).isEqualTo("");
+	}
+
+	@Test
+	public void testStringOfOneAlphaCharAndShiftOf1() {
+		CaesarShift caesarShift = new CaesarShift(1);
+		String result = caesarShift.encrypt("a");
+		assertThat(result).isEqualTo("b");
+	}
+
+	@Test
+	public void testStringOfzAndShiftOf1() {
+		CaesarShift caesarShift = new CaesarShift(1);
+		String result = caesarShift.encrypt("z");
+		assertThat(result).isEqualTo("a");
+	}
+
+	@Test
+	public void testStringOfBAndShiftOf1() {
+		CaesarShift caesarShift = new CaesarShift(1);
+		String result = caesarShift.encrypt("B");
+		assertThat(result).isEqualTo("C");
+	}
+
+	@Test
+	public void testStringOfSpaceAndShiftOf1() {
+		CaesarShift caesarShift = new CaesarShift(1);
+		String result = caesarShift.encrypt(" ");
+		assertThat(result).isEqualTo(" ");
+	}
+
+	public void testStringOfSpace() {
+	}
+
+	public void testStringOfSpecialChars() {
+	} // ~'$#@
+
+	@Test
+	public void testCaesarShift0WithANullString() {
+		CaesarShift caesarShift = new CaesarShift(0);
+		try {
+			caesarShift.encrypt(""); // Exception!
+			fail("The line should not reach this point");
+		} catch (IllegalArgumentException iae) {
+			assertThat(iae).hasMessage("String cannot be null");
+		}
+	}
+
+	public void testDoWeShiftNumbers() {
+	}
+
+	public void testNegativeShift() {
+	}
 }
