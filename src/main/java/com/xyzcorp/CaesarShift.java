e8d57cdb2df4ccfbc31c2e9412fc9e6bb5f775f3
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-06-22 13:25:44 -0600

    Last parts of our test!

--- src/main/java/com/xyzcorp/CaesarShift.java
+++ src/main/java/com/xyzcorp/CaesarShift.java
@@ -1,11 +1,21 @@
 package com.xyzcorp;
 
-public class CaesarShift {
-    public CaesarShift(int shift) {
+import java.util.Objects;
 
+public class CaesarShift {
+    private final int shift;
+
+    public CaesarShift(int shift) {
+        this.shift = shift;
     }
 
     public String encode(String s) {
-        return "";
+        Objects.requireNonNull(s, "String is null");
+        if (s.isEmpty()) return "";
+        //int actualShift = s.charAt(0) + shift;
+        //if (actualShift > 122) actualShift = actualShift - 122 + 96;
+        return Character.toString((char) ((s.charAt(0) - 97 + shift) % 26 + 97));
+        //123
+        //
     }
 }
--- src/test/java/com/xyzcorp/CaesarShiftTest.java
+++ src/test/java/com/xyzcorp/CaesarShiftTest.java
@@ -2,13 +2,20 @@ package com.xyzcorp;
 
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import static junit.framework.TestCase.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import org.junit.Rule;
+
 public class CaesarShiftTest {
     
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
 	public void testShiftOf0OnAnEmptyString() {
 		//I always want the simplest thing first.
@@ -30,9 +37,91 @@ public class CaesarShiftTest {
 			cs.encode(null);
 			fail("This line should never be executed");
 		} catch (NullPointerException e) {
-			assertTrue(true);
+			assertThat(e).hasMessage("String is null");
 		}
 	}
 
-	public void testSingleLetterShiftOf0() {}
+	@Test
+	public void testNullStringExpectedException() {
+		thrown.expect(NullPointerException.class);
+		thrown.expectMessage("String is null");
+		CaesarShift cs = new CaesarShift(0);
+		cs.encode(null);
+	}
+
+	@Test
+	public void testNullStringAssertJTrick() {
+		CaesarShift cs = new CaesarShift(0);
+		assertThatThrownBy(() -> cs.encode(null))
+		.isInstanceOf(NullPointerException.class)
+		.hasMessage("String is null");
+	}
+
+	@Test
+	public void testSingleLetterShiftOf0() {
+		CaesarShift cs = new CaesarShift(0);
+		assertThat(cs.encode("a")).isEqualTo("a");
+	}
+
+	@Test
+	public void testSingleLetterShiftOf1() {
+		CaesarShift cs = new CaesarShift(1);
+		assertThat(cs.encode("a")).isEqualTo("b");
+	}
+
+	//NoRed
+	@Test
+	public void testSingleLetterShiftOf5FromCTo() {
+		CaesarShift cs = new CaesarShift(5);
+		assertThat(cs.encode("c")).isEqualTo("h");
+	}
+
+	/// a = 97 ... z = 122
+	/// a..z = 26
+
+	// 122 + 1 % 2
+
+	@Test
+	public void testSingleLetterShiftOf1FromZ() {
+		CaesarShift cs = new CaesarShift(1);
+		assertThat(cs.encode("z")).isEqualTo("a");
+	}
+
+	@Test
+	public void testSingleLetterShiftOf5FromX() {
+		CaesarShift cs = new CaesarShift(5);
+		assertThat(cs.encode("x")).isEqualTo("c");
+	}
+
+	@Test
+	public void testSingleLetterShiftOf26AndShouldBeTheSameLetter() {
+		CaesarShift cs = new CaesarShift(26);
+		assertThat(cs.encode("p")).isEqualTo("p");
+	}
+
+	@Test
+	public void testSingleLetterShiftOf27AndShouldBeTheSameAsAShiftOf1() {
+		CaesarShift cs = new CaesarShift(27);
+		assertThat(cs.encode("p")).isEqualTo("q");
+	}
+
+	@Test
+	public void testSingleLetterShiftOf104AndShouldBeUnshifted() {
+		CaesarShift cs = new CaesarShift(26 * 4);
+		assertThat(cs.encode("p")).isEqualTo("p");
+	}
+
+	@Test
+	public void testSingleLetterShiftOf123AndShouldBeShiftedT() {
+		CaesarShift cs = new CaesarShift(123);
+		assertThat(cs.encode("p")).isEqualTo("i"); //i?
+	}
+
+	//Homework!
+	//123 problem
+	//Add another character
+	//Capital Letters should work: A + 5 = F, Z + 4 = D
+	//Add other characters to make complete sentences
+	//Spaces, Numbers, Strange Characters (Leave them alone)
+	//Decode (subtraction)
 }
