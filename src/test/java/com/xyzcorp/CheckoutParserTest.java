14dfdda61f901dbe6ddc2ca30a58e0344a180b2e
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-06-23 15:27:37 -0600

    Cleaned up imports on CheckoutParserTest.

--- src/test/java/com/xyzcorp/CheckoutParserTest.java
+++ src/test/java/com/xyzcorp/CheckoutParserTest.java
@@ -2,15 +2,9 @@ package com.xyzcorp;
 
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.Arrays;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
