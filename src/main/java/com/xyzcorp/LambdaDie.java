91786b8b6d2611636bf8e8a97cfdc3b3ae0ba057
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-06-23 11:05:01 -0600

    Added Die example for mocking!

---
+++ src/main/java/com/xyzcorp/LambdaDie.java
@@ -0,0 +1,16 @@
+package com.xyzcorp;
+
+import java.util.function.Supplier;
+
+public class LambdaDie {
+    public LambdaDie(Supplier<Integer> supplier) {
+    }
+
+    public LambdaDie roll() {
+        return null;
+    }
+
+    public int getPips() {
+        return 0;
+    }
+}
