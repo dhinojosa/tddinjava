5b325979e4b4d8188e9000ba55a8763725774b3a
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2018-06-01 13:01:49 -0600

    Today's updates

--- src/main/java/com/xyzcorp/CaesarShift.java
+++ src/main/java/com/xyzcorp/CaesarShift.java
@@ -1,28 +1,26 @@
 package com.xyzcorp;
 
 import java.util.Objects;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CaesarShift {
 
-	public static String encode(String input, int shift) {
-		Objects.requireNonNull(input, "Input cannot be null");
-		if (input.isEmpty()) return input;
-		return input.chars()
-		     .boxed()
-		     .map(i -> shiftChar(shift, (char)(i.intValue())))
-		     .collect(Collectors.joining());
-	}
+    public static String encode(String input, int shift) {
+        Objects.requireNonNull(input, "Input cannot be null");
+        if (input.isEmpty()) return input;
+        return input.chars()
+                    .boxed()
+                    .map(i -> String.valueOf((char) shiftChar(shift, i)))
+                    .collect(Collectors.joining());
+    }
 
-	private static String shiftChar(int shift, char c) {
-		int afterShift = c + shift;
-		if (afterShift > 122) {
-			return String.valueOf((char) ((afterShift % 122) + 96));
-		} else {
-			return String.valueOf((char) (afterShift));
-		}
-		
-		//return String.valueOf((char)(((input.charAt(0) + shift) - 97) % 26 + 97));
-	}
+    private static int shiftChar(int shift, int c) {
+        if (!Character.isAlphabetic(c)) return c;
+        char a = Character.isUpperCase(c) ? 'A' : 'a';
+        return ((c + (shift % 26 + 26) % 26 - a) % 26 + a);
+    }
+
+    public static String decode(String input, int shift) {
+        return encode(input, -shift);
+    }
 }
